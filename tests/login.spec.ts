f659a80925854da367e633ede10817d10dd25b3f
Hossam-Shehadeh <hossam.h.shehadeh@gmail.com>
2025-05-25 22:25:30 +0300

    add login tests for valid user credentials

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,6 +1,11 @@
-import { test } from '@playwright/test';
-
+import { test, expect } from '@playwright/test';
+import { LoginPage } from '../pages/LoginPage';
 
 test.describe('Login Feature', () => {
- 
+  test('Login with valid credentials', async ({ page }) => {
+    const login = new LoginPage(page);
+    await login.goto();
+    await login.login('standard_user', 'secret_sauce');
+    await expect(page).toHaveURL(/inventory.html/);
+  });
 });
