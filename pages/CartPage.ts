8bc97a7884758f692c87b5bae7957d72ee54fb72
Hossam-Shehadeh <hossam.h.shehadeh@gmail.com>
2025-05-25 22:27:45 +0300

    add test for adding backpack to cart

--- pages/CartPage.ts
+++ pages/CartPage.ts
@@ -3,5 +3,11 @@ import { Page } from '@playwright/test';
 export class CartPage {
   constructor(private page: Page) {}
 
-  
+  async removeItem() {
+    await this.page.click('[data-test^="remove-"]');
+  }
+
+  async clickCheckout() {
+    await this.page.click('[data-test="checkout"]');
+  }
 }
